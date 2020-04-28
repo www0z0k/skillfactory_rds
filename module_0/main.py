91904bde4c87c87f6c3dc65322e0d4d639319570
www0z0k <www0z.step@gmail.com>
2020-04-28 04:17:50 +0300

    moved range to top to a variable, added some spaces to beautify the code

--- module_0/main.py
+++ module_0/main.py
@@ -1,13 +1,15 @@
 import numpy as np
 import math
 
+MAX_VAL = 100
+
 def game_core_v1(number):
     '''Просто угадываем на random, никак не используя информацию о больше или меньше.
        Функция принимает загаданное число и возвращает число попыток'''
     count = 0
     while True:
-        count+=1
-        predict = np.random.randint(1,101) # предполагаемое число
+        count += 1
+        predict = np.random.randint(1, MAX_VAL + 1) # предполагаемое число
         if number == predict: 
             return(count) # выход из цикла, если угадали
         
@@ -16,7 +18,7 @@ def score_game(game_core):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1,101, size=(1000))
+    random_array = np.random.randint(1, MAX_VAL + 1, size = (1000))
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
@@ -30,9 +32,9 @@ def game_core_v2(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
     count = 1
-    predict = np.random.randint(1,101)
+    predict = np.random.randint(1, MAX_VAL + 1)
     while number != predict:
-        count+=1
+        count += 1
         if number > predict: 
             predict += 1
         elif number < predict: 
@@ -45,10 +47,10 @@ def game_core_v2(number):
 def game_core_v3(number):
     # try to play with prediction step value
     count = 1
-    step = 100
-    predict = np.random.randint(1,101)
+    step = MAX_VAL
+    predict = np.random.randint(1, MAX_VAL + 1)
     while number != predict:
-        count+=1
+        count += 1
         step = math.ceil(step / 2)
         if number > predict: 
             predict += step
