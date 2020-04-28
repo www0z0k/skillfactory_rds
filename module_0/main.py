d542f9ab8941f27954c8acbf56e842b2e2054009
www0z0k <www0z.step@gmail.com>
2020-04-28 04:13:34 +0300

    added v3 with variable step

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,59 @@
-print("Hello world")
+import numpy as np
+import math
+
+def game_core_v1(number):
+    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count+=1
+        predict = np.random.randint(1,101) # предполагаемое число
+        if number == predict: 
+            return(count) # выход из цикла, если угадали
+        
+        
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+# запускаем
+# score_game(game_core_v1)
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1,101)
+    while number != predict:
+        count+=1
+        if number > predict: 
+            predict += 1
+        elif number < predict: 
+            predict -= 1
+    return(count) # выход из цикла, если угадали
+
+# Проверяем
+# score_game(game_core_v2)
+
+def game_core_v3(number):
+    # try to play with prediction step value
+    count = 1
+    step = 100
+    predict = np.random.randint(1,101)
+    while number != predict:
+        count+=1
+        step = math.ceil(step / 2)
+        if number > predict: 
+            predict += step
+        elif number < predict: 
+            predict -= step
+    return(count) # выход из цикла, если угадали
+
+score_game(game_core_v3)
